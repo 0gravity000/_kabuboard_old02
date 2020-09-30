a8df2a9db7aa6432565380f7f1123a76102f1477
0gravity000 <0gravity000@gmail.com>
2020-09-30 21:45:21 +0900

    10日分を残し削除する処理がうまく動かないため、仮の処理index_debug()として追加
    Changes to be committed:
    modified:   app/Http/Controllers/SignalController.php

--- app/Http/Controllers/SignalController.php
+++ app/Http/Controllers/SignalController.php
@@ -302,7 +302,28 @@ class SignalController extends Controller
 
     public function index_debug()
     {
+        $daily_history = DailyHistory::orderBy('created_at', 'asc')->first();
+        $createddate = $daily_history->created_at;
 
+        //現在
+        $now = Carbon::now(new DateTimeZone('Asia/Tokyo'));
+        //今日の日付
+        $today = Carbon::create($now->year, $now->month, $now->day, 00, 00, 01);
+        //基準日の日付 10日分
+        $branchday = $today;
+        for ($i = 0; $i < 10; $i++) { 
+            $branchday = $branchday->subDay();
+            //var_dump($branchday);
+        }
+        //dd($branchday);
+
+        $daily_historys = DailyHistory::where('created_at', '<', $branchday)->get();
+        //dd($daily_historys);
+        foreach ($daily_historys as $daily_history) {
+            $daily_history->delete();
+        }
+
+        return view('signal');
     }
 
     /**
