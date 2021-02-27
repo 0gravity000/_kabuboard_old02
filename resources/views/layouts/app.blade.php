abdf06c64075b8d5fe0b98cd56914012088d95d6
0gravity000 <0gravity000@gmail.com>
2021-02-28 04:50:37 +0900

    Google Adsenseを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script data-ad-client="ca-pub-6897468555074184" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
